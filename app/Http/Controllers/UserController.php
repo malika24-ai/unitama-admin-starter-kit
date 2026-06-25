6e00088728080f2199ad986e5dea7501ccaa8e3c
lika <malika24@mhs.unitama.ac.id>
2026-06-26 00:24:58 +0800

    pertemuan 12: User Create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -12,7 +14,8 @@ class UserController extends Controller
     public function index()
     {
         return view('user.index',[
-            'title'=> 'User'
+            'title'=> 'User',
+            'users' => User::latest()->get()
         ]);
     }
 
@@ -21,7 +24,10 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('user.create',[
+            'title'=> 'Tambah User',
+            
+        ]);
     }
 
     /**
@@ -29,7 +35,38 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+    $validated = $request->validate([
+    'name' => 'required|string|max:255',
+    'email' => 'required|email|max:255|unique:users',
+    'password' => 'required|min:8',
+    'passwordconfirm' => 'required|same:password',
+    'avatar' => 'nullable|image|max:2048',
+    'role' => 'required|in:Superadmin,Admin',
+], [
+    'required' => ':Attribute wajib diisi.',
+    'email' => 'Format email tidak valid.',
+    'unique' => 'Email sudah terdaftar.',
+    'min' => ':Attribute minimal :min karakter.',
+    'max' => ':Attribute maksimal :max karakter.',
+    'image' => 'File harus berupa gambar.',
+    'in' => 'Role tidak valid.',
+]);
+
+if($request->file('avatar')){
+    $validated['avatar'] = $request->file('avatar')->store('avatar', 'public');
+}
+
+    try {
+        DB::beginTransaction();
+        User::create($validated);
+        DB::commit();
+        return to_route('user.index')->withSuccess('Data berhasil ditambahkan');
+
+    }catch (\Exception $e) {
+        DB::rollBack();
+        return to_route('user.create')->withError('Data gagal ditambahkan');
+    }
+
     }
 
     /**
